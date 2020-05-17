d77ee33541f0579b17bfe27c491c7ebdcaa813ae
Ferenc Hechler <github@fh.anderemails.de>
2020-05-17 22:18:13 +0200

    added modified sources from pxt-neural-network-json, locally runnable.

---
+++ src/NeuralNetsMinGWProject.cpp
@@ -0,0 +1,40 @@
+//============================================================================
+// Name        : FerisFirstMinGWProject.cpp
+// Author      : feri
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+#include "neuralnetwork.h"
+#include "platform/utils.h"
+#include "neuralnets/Vect.h"
+#include "neuralnets/NN.h"
+
+using namespace std;
+
+
+int main() {
+	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+
+	Vect hidden = createVect(2, 10.0f, 10.0f);
+
+	nn::initfcnn(3, &hidden, 2);
+
+	Vect x = createVect(3, 2.0f, 3.0f ,4.0f);
+	Vect y = createVect(2, 1.0f, 0.0f);
+
+	float err = nn::train(&x, &y);
+	logFloat(err);
+
+	nn::predict(&x, &y);
+	logFloat(y.get(0));
+	log(", ");
+	logFloat(y.get(1));
+
+	nn::getBrain()->print();
+
+	log("\nfinished");
+	return 0;
+}
